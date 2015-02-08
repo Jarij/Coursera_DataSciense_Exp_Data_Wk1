7e970b060fea3d3dd4fe13c383095f279c2f6f5d
Jarij <juntu2@hotmail.com>
2015-02-08 15:03:26 -0800

    Create load_data.R
    List of commands to manipulate the original data.
    Data Set has been created according to instructions for 2007-02-02 and 2007-02-01 only. This data will be used for plotting purposes.

---
+++ load_data.R
@@ -0,0 +1,22 @@
+# In Windows 8 automatic file download to "Downloads" - directory
+# I didn't have time find correct parameters to be changed to get that working better - mayby next time
+
+# Set working directory
+setwd("C://Users//jari//Data_Science//working_Dir")
+
+# Manually data extracted to working directory -> household_power_consumption.txt 
+
+library(datasets)
+myTable <- read.table("C://Users//jari//Data_Science//working_Dir//household_power_consumption.txt", header = TRUE, sep=";", colClasses = c("character", "character", rep("numeric",7)),na="?")
+
+# Check the column names and structure
+head(myTable)
+
+# Date Structure update 16/12/2006 -> 2006-12-16 as given in the instructions
+myTable$Date <- as.Date(myTable$Date, "%d/%m/%Y")
+
+# Selected dates 2007-02-01 and 2007-02-02
+SelDates <- as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
+
+# Final data for plotting purposes
+finalData <- subset(myTable, Date %in% SelDates)
